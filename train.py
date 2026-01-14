c9e3c8b5f0f99e594d6a948ddc81d28cd07724d5
satellitelmk <592752638@qq.com>
2026-01-14 18:18:39 +0800

    Update fused_split_new function call in training process

--- train.py
+++ train.py
@@ -474,6 +474,7 @@ def get_reco_loss(args, task, base_model, base_auxiliary,graphs,match_graph,devi
 
 
     models = reco_training_inner(args, task, base_model, base_auxiliary, fused_graph,graphs,device)
+    fused_graph,graphs = fused_split_new(fused_graph,graphs,task)
 
   
     cum = 0
